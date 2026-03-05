8c5adb37d800f24226646ed4730416d597a039f0
Andrew Allemond <118858494+aallemond@users.noreply.github.com>
2026-03-04 19:54:09 -0600

    changed logo src in manifest.json

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,33 +4,13 @@ import PriceTable from "../components/PriceTable";
 import About from "../components/About";
 import Hero from "../components/Hero";
 import Contact from "../components/Contact";
+import {metals} from "../data/metals"
 
 
-import copper from "../data/copper.json";
-import brass from "../data/brass.json";
-import aluminum from "../data/aluminum.json";
-import stainless from "../data/stainless.json";
-import cpus from "../data/cpu.json";
-import misc from "../data/misc.json";
-import ram from "../data/ram.json";
-import escrap from "../data/escrap.json";
-import whole from "../data/whole.json";
+
 
 
 export default function Home() {
-
-  const metals = [
-    { id:"copper", title: "Copper", data: copper },
-    { id: "brass", title: "Brass", data: brass },
-    { id: "aluminium",title: "Aluminum", data: aluminum },
-    { id:"stainless",title: "Stainless Steel", data: stainless },
-    { id:"escrap", title: "E-Scrap", data: escrap },
-    { id:"whole",title: "Whole Scrap", data: whole },
-    { id:"cpus",title: "CPUs", data: cpus },
-    { id:"ram", title: "RAM", data: ram },
-    { id:"misc",title: "Misc.", data: misc }
-
-  ];
 
   return (
     <div>
