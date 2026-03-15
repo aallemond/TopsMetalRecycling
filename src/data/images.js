a1298fd1a64962e30d6f33e0c30650aa89f2f95a
Andrew Allemond <118858494+aallemond@users.noreply.github.com>
2026-03-15 13:35:04 -0500

    adjusted case for vercel/new build

--- src/data/images.js
+++ src/data/images.js
@@ -1,3 +1,5 @@
-export default[
-    "https://res.cloudinary.com/dxnwbl21t/image/upload/v1767588391/333D4419-ADE8-4E3A-8572-9E336FF1149C_ffqojx.png"
+const images = [
+  "https://res.cloudinary.com/dxnwbl21t/image/upload/v1767588391/333D4419-ADE8-4E3A-8572-9E336FF1149C_ffqojx.png"
 ];
+
+export default images;
